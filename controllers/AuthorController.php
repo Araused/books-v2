6caacbf13bf6d44f2f5bff71be00b0004c328362
Nikolay Pokrova <n.pokrova@qksr.ru>
2026-04-25 17:59:54 +0500

    AuthorController

---
+++ controllers/AuthorController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace app\controllers;
+
+use Yii;
+use app\models\Author;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
+use yii\filters\AccessControl;
+use yii\data\ActiveDataProvider;
+use yii\web\Response;
+
+class AuthorController extends Controller
+{
+    public function behaviors(): array
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::class,
+                'only' => ['create', 'update', 'delete'],
+                'rules' => [
+                    [
+                        'actions' => ['create', 'update', 'delete'],
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+            'verbs' => [
+                'class' => VerbFilter::class,
+                'actions' => [
+                    'delete' => ['POST'],
+                ],
+            ],
+        ];
+    }
+
+    public function actionIndex(): string
+    {
+        $dataProvider = new ActiveDataProvider([
+            'query' => Author::find(),
+            'pagination' => ['pageSize' => 20],
+        ]);
+
+        return $this->render('index', [
+            'dataProvider' => $dataProvider,
+        ]);
+    }
+
+    public function actionView(int $id): string
+    {
+        return $this->render('view', [
+            'model' => $this->findModel($id),
+        ]);
+    }
+
+    /**
+     * @throws \Throwable
+     */
+    public function actionCreate(): string|Response
+    {
+        $model = new Author();
+
+        if ($this->request->isPost && $model->load($this->request->post()) && $model->save()) {
+            return $this->redirect(['view', 'id' => $model->id]);
+        }
+
+        return $this->render('create', [
+            'model' => $model,
+        ]);
+    }
+
+    /**
+     * @throws \Throwable
+     */
+    public function actionUpdate(int $id): string|Response
+    {
+        $model = $this->findModel($id);
+
+        if ($this->request->isPost && $model->load($this->request->post()) && $model->save()) {
+            return $this->redirect(['view', 'id' => $model->id]);
+        }
+
+        return $this->render('update', [
+            'model' => $model,
+        ]);
+    }
+
+    public function actionDelete(int $id): Response
+    {
+        $this->findModel($id)->delete();
+
+        return $this->redirect(['index']);
+    }
+
+    /**
+     * @throws NotFoundHttpException
+     */
+    protected function findModel(int $id): Author
+    {
+        if (($model = Author::findOne($id)) !== null) {
+            return $model;
+        }
+
+        throw new NotFoundHttpException('Запрошенная страница не существует.');
+    }
+}
+
