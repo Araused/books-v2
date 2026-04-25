820d2fb9cf528fb9c3a14b35c439b14d9421d804
Nikolay Pokrova <n.pokrova@qksr.ru>
2026-04-25 13:31:24 +0500

    db structure

---
+++ migrations/m260425_074621_init.php
@@ -0,0 +1,61 @@
+<?php
+
+use yii\db\Migration;
+
+class m260425_074621_init extends Migration
+{
+    public function safeUp(): void
+    {
+        $this->createTable('{{%book}}', [
+            'id' => $this->primaryKey(),
+            'author_id' => $this->integer()->notNull(),
+            // 13 символов на сам книжный код + 4 возможных дефиса. У одной и той же книги с разными годами издания должны быть разные ISBN - поэтому поле UNIQUE
+            'isbn' => $this->string(17)->notNull()->unique(),
+            'title' => $this->string()->notNull(),
+            'publish_date' => $this->dateTime()->notNull(),
+            'preview' => $this->string(),
+            'image' => $this->string(),
+        ]);
+
+        $this->createTable('{{%author}}', [
+            'id' => $this->primaryKey(),
+            'firstname' => $this->string()->notNull(),
+            'lastname' => $this->string()->notNull(),
+            'middlename' => $this->string(),
+        ]);
+
+        $this->createTable('{{%author_subscription}}', [
+            'id' => $this->primaryKey(),
+            'author_id' => $this->integer()->notNull(),
+            // Для удобства хранения стандартных российских сотовых номеров: 11 цифр вместе с плюсом в начале
+            'phone' => $this->string(12)->notNull(),
+        ]);
+
+        $this->addForeignKey(
+            'fk_book_to_author',
+            '{{%book}}',
+            'author_id',
+            '{{%author}}',
+            'id',
+            'CASCADE',
+            'CASCADE'
+        );
+
+        $this->addForeignKey(
+            'fk_subscription_to_author',
+            '{{%author_subscription}}',
+            'author_id',
+            '{{%author}}',
+            'id',
+            'CASCADE',
+            'CASCADE'
+        );
+    }
+
+    public function safeDown(): void
+    {
+        $this->dropTable('{{%book}}');
+        $this->dropTable('{{%author_subscription}}');
+        $this->dropTable('{{%author}}');
+    }
+}
